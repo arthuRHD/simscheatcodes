16528bf84e94ca29c97bb919e690e80bfce2c9ef
Arthur RICHARD <arthur.richard@protonmail.com>
2021-02-07 19:05:30 +0100

    icons

---
+++ public/analytics.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDJc3IfKrImz-2HwrIeF1tMugfMO6XKcfs",
+    authDomain: "sims4-cheatcodes.firebaseapp.com",
+    projectId: "sims4-cheatcodes",
+    storageBucket: "sims4-cheatcodes.appspot.com",
+    messagingSenderId: "613933431000",
+    appId: "1:613933431000:web:b91cc699055850e783bfad",
+    measurementId: "G-66BZFYT93R"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
